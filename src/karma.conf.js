de3b8fbdf3f0dcefcc94174b83af0bdcf5678245
flanjersi <grosj83670@gmail.com>
2019-02-28 12:28:08 +0100

    prepare to jenkis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      // require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -15,18 +15,36 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    // UPDATE
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    // END UPDATE
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/genrest-front-end'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+      ? ['progress', 'coverage-istanbul']
+      : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    //browsers: ['Chrome'],
+    browsers: ['PhantomJS'], /*remove chrome and replace it with PhantomJS */
+    //singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
