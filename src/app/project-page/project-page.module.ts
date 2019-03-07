e8a37212ee7a1f2511e4aee5342bbc8e6ae86738
tsila <ainatsilavina007@gmail.com>
2019-03-07 17:08:50 +0100

    Composant tableur

--- src/app/project-page/project-page.module.ts
+++ src/app/project-page/project-page.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProjectPageComponent} from './project-page.component';
 import {RouterModule, Routes} from "@angular/router";
-
+import { TabSpecComponent } from './tab-spec/tab-spec.component';
+import {MaterialModule} from "../features/material.module";
 
 const routes: Routes = [
   { path: '', component: ProjectPageComponent},
 ];
 @NgModule({
-  declarations: [ProjectPageComponent],
+  declarations: [ProjectPageComponent, TabSpecComponent],
   imports: [
+    MaterialModule,
     CommonModule,
     RouterModule.forChild(routes)
   ]
