688f4db71ec7b2b567a1ebe4d3c9beae3231c184
ramzi-guellil <brixyes@gmail.com>
2019-03-14 09:31:19 +0100

    integration in project page of tree view

--- src/app/project-page/project-page.module.ts
+++ src/app/project-page/project-page.module.ts
@@ -8,20 +8,23 @@ import {MenuProjectComponent} from './menu-project/menu-project.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import { GraphSpecComponent } from './graph-spec/graph-spec.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { MytreeComponent } from './mytree/mytree.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { SharedModule } from '../shared/shared.module';
 
 
 const routes: Routes = [
   { path: '', component: ProjectPageComponent},
 ];
 @NgModule({
-  declarations: [ProjectPageComponent, TabSpecComponent, MenuProjectComponent, GraphSpecComponent],
+  declarations: [ProjectPageComponent, TabSpecComponent, MenuProjectComponent, GraphSpecComponent, MytreeComponent],
   imports: [
     MaterialModule,
     CommonModule,
     NgxGraphModule,
     NgxChartsModule,
     MDBBootstrapModule.forRoot(),
+    SharedModule.forRoot(),
     RouterModule.forChild(routes)
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
