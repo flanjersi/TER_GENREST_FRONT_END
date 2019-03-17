91679c62dd83bb6f4d1891d5a3b0e19d86fb5368
abdoudrums <abderahimhachemi@hotmail.com>
2019-03-18 00:15:00 +0100

    update project-spec

--- src/app/project-page/tree-view-specifications/tree-view-specifications.component.ts
+++ src/app/project-page/tree-view-specifications/tree-view-specifications.component.ts
@@ -136,6 +136,8 @@ export class TreeViewSpecificationsComponent implements OnInit, OnChanges {
     return data;
   }
 
+
+
   generateBuilding(building: Building): any {
     const buildingData = {};
 
